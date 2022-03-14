1bbee3160edfdb5846158357d88b75aedc6fb7f6
art <art@wifilapperhome>
2022-03-13 21:23:42 -0600

    Race is now fully joinable!

--- tourjs-react/src/Components/InRaceView.tsx
+++ tourjs-react/src/Components/InRaceView.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useRef } from "react";
+import { tickGameAnimationFrame } from "../AppTestHacks";
+import { DecorationFactory } from "../tourjs-client-shared/DecorationFactory";
+import { DecorationState } from "../tourjs-client-shared/DecorationState";
+import { defaultThemeConfig } from "../tourjs-client-shared/drawing-constants";
+import { createDrawer } from "../tourjs-client-shared/drawing-factory";
+import { PaintFrameState } from "../tourjs-client-shared/drawing-interface";
+import { ServerMapDescription } from "../tourjs-shared/communication";
 import { RaceState } from "../tourjs-shared/RaceState";
+import { PureCosineMap } from "../tourjs-shared/RideMap";
+import { RideMapHandicap } from "../tourjs-shared/RideMapHandicap";
+import { FakeUserProvider, setupRace } from "../UtilsGameStarter";
+import './InRaceView.scss';
 
 export default function InRaceView(props:{raceState:RaceState}) {
   const user = props.raceState.getUserProvider().getLocalUser();
+  const canvasRef = useRef<HTMLCanvasElement>();
   
-  return <div>It's the in-race view!  You're at {user && user.getDistance().toFixed(1)}m</div>
+  useEffect(() => {
+    // startup: let's get our game state up and running
+    if(canvasRef?.current) {
+      canvasRef.current.width = canvasRef?.current?.clientWidth;
+      canvasRef.current.height = canvasRef?.current?.clientHeight;
+      const drawer = createDrawer("3d");
+
+      const decFactory = new DecorationFactory(defaultThemeConfig);
+      const decState = new DecorationState(props.raceState.getMap(), decFactory);
+      const paintState = new PaintFrameState();
+      
+      requestAnimationFrame((tm) => tickGameAnimationFrame(tm, tm, drawer, decState, paintState, canvasRef, props.raceState, ()=>{}));
+    }
+  }, [canvasRef]);
+  
+  return <div>
+    <p>It'z the in-race view!  You're at {user && user.getDistance().toFixed(1)}m</p>
+    <canvas ref={canvasRef}  className="InRaceView__Canvas"/>
+    </div>
 }
