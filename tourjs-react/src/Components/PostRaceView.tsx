b3bb3f08b5f95f3a51167c88b3625cc61495b14e
art <art@wifilapperhome>
2022-09-25 17:00:10 -0600

    Stuff

--- tourjs-react/src/Components/PostRaceView.tsx
+++ tourjs-react/src/Components/PostRaceView.tsx
@@ -15,7 +15,7 @@ interface RaceResults {
   types:number[];
 }
 
-function RankingList(props:{title:string, names:string[], values:number[], bigToLittle:boolean, formatValues:(val:number, index:number)=>string, formatClass:(index:number)=>string[]}) {
+function RankingList(props:{title:string, names:string[], values:number[], bigToLittle:boolean, formatValues:(val:number, unsortedIndex:number, sortedIndex:number)=>string, formatClass:(index:number)=>string[]}) {
 
   assert2(props.values.length === props.names.length);
 
@@ -35,7 +35,7 @@ function RankingList(props:{title:string, names:string[], values:number[], bigTo
     <h3>{props.title}</h3>
     <ul>
       {comboVals.map((row, index) => {
-        return <li className={`RankingList__Item ${props.formatClass(row.orgIdx).join(' ')}`}>{props.formatValues(row.val, row.orgIdx)}: {row.name}</li>
+        return <li className={`RankingList__Item ${props.formatClass(row.orgIdx).join(' ')}`}>{props.formatValues(row.val, row.orgIdx, index)}: {row.name}</li>
       })}
     </ul>
   </>)
@@ -46,11 +46,16 @@ export default function PostRaceView(props:{raceResults:RaceResults}) {
 
   function userSpendingKeyToString(key:string):string {
     switch(key) {
-      case "whole-course": return "Whole Course Spending";
-      case "while-downhill": return "While Downhill Spending";
-      case "first-half": return "First Half Spending";
-      case "last-500m": return "Last 500m Spending";
-      case "last-half": return "Second Half Heroes";
+      case "whole-course": return "Whole Course Spending (total)";
+      case "while-downhill": return "While Downhill Spending (total)";
+      case "while-uphill": return "While Uphill (total)";
+      case "first-half": return "First Half Spending (total)";
+      case "last-half": return "Second Half Spending (total)";
+      case "last-500m": return "Last 500m Spending (/total)";
+      default:
+        console.warn("Don't know good string for " + key);
+        debugger;
+        return key;
     }
   }
 
@@ -77,17 +82,36 @@ export default function PostRaceView(props:{raceResults:RaceResults}) {
     return classes;
   }
 
+  const formatValuesForFinishOrder = (val:number, orgIdx:number, sortedIndex:number) => {
+    const finishTime = props.raceResults.times[orgIdx];
+    let timeValue = '';
+    if(finishTime < 0 || finishTime === Number.MAX_SAFE_INTEGER) {
+      timeValue = 'DNF';
+    } else {
+      timeValue = formatSecondsHms(props.raceResults.times[orgIdx]);
+    }
+
+    return '#' + (sortedIndex+1) + ' - ' + timeValue;
+  }
+
+  const filteredTimes = props.raceResults.times.map((time) => {
+    if(time <= 0) {
+      time = Number.MAX_SAFE_INTEGER;
+    }
+    return time;
+  })
   return <div className="PostRaceView__Container">
     <h2>You've finished a {props.raceResults.raceLengthKm.toFixed(1)}km Race!</h2>
     <p>Permalink: <a href={permalink}>Permalink</a></p>
-    <RankingList title="Who Won? Finishing Order" formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.rankings} bigToLittle={false} formatValues={(val:number, index:number) => "#" + (val+1) + " - " + formatSecondsHms(props.raceResults.times[index])} />
+    <RankingList title="Who Won? Finishing Order" formatClass={formatClass} names={props.raceResults.names} values={filteredTimes} bigToLittle={false} formatValues={formatValuesForFinishOrder} />
 
-    <RankingList title="Effort: FTP-Seconds Spent" formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.efficiency} bigToLittle={true} formatValues={(val:number, index:number) => val.toFixed(0) + "/km"} />
+    <RankingList title="Effort: FTP-Seconds Spent Per Km" formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.efficiency} bigToLittle={true} formatValues={(val:number, index:number, sortedIndex:number) => val.toFixed(0) + "/km"} />
     
-    <RankingList title="Drafting: FTP-Seconds Saved " formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.hsSaved} bigToLittle={true} formatValues={(val:number, index:number) => val.toFixed(1)} />
+    <RankingList title="Drafting: FTP-Seconds Saved " formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.hsSaved} bigToLittle={true} formatValues={(val:number, index:number, sortedIndex:number) => val.toFixed(1)} />
     
+    <h4>Total Spending In Key Scenarios</h4>
     {Object.keys(props.raceResults.userSpending[0]).map((key, index) => {
-      return <RankingList title={userSpendingKeyToString(key)} formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.userSpending.map((us) => us[key])} bigToLittle={true} formatValues={(val:number, index:number) => val.toFixed(1)} />
+      return <RankingList title={userSpendingKeyToString(key)} formatClass={formatClass} names={props.raceResults.names} values={props.raceResults.userSpending.map((us) => us[key])} bigToLittle={true} formatValues={(val:number, index:number, sortedIndex:number) => val?.toFixed(1) || "N/A"} />
     })}
   </div>
 }
